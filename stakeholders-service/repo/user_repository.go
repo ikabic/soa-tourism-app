6e8bbf967fb21333ed34f42bc6a989f563851538
Svetlana Kis <svetlanakis2003@gmail.com>
2026-04-08 18:30:36 +0200

    feat(stakeholders): add UserRepository with create and find by methods

---
+++ stakeholders-service/repo/user_repository.go
@@ -0,0 +1,26 @@
+package repo
+
+import (
+	"gorm.io/gorm"
+	"stakeholders-service.xws.com/model"
+)
+
+type UserRepository struct {
+	DB *gorm.DB
+}
+
+func (repo *UserRepository) Create(user *model.User) error {
+	return repo.DB.Create(user).Error
+}
+
+func (repo *UserRepository) FindByUsername(username string) (*model.User, error) {
+	var user model.User
+	err := repo.DB.Where("username = ?", username).First(&user).Error
+	return &user, err
+}
+
+func (repo *UserRepository) FindByEmail(email string) (*model.User, error) {
+	var user model.User
+	err := repo.DB.Where("email = ?", email).First(&user).Error
+	return &user, err
+}
